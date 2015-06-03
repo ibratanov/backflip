b42047a78e59c4254408c25361b4c85f6a8a9e42
Jonathan Arlauskas <jonathan.arlauskas@gmail.com>
2015-06-03 13:58:15 -0400

    Button switched view from like sort to time sort. Multiple view options WIP
    Hitting the sort button switches the album view to order by either most likes, or by time

--- ParseStarterProject/albumViewController.swift
+++ ParseStarterProject/albumViewController.swift
@@ -33,20 +33,6 @@ class albumViewController: UICollectionViewController {
     var sorted = false
     var sorted1 = true
 
-    func sortButton(sender: AnyObject) {
-        
-        if sorted1 == true {
-        
-            sorted1 = false
-            println(sorted1)
-            
-        } else {
-            
-            sorted1 = false
-            println(sorted1)
-        }
- 
-    }
     
     
     override func viewDidLoad() {
@@ -60,14 +46,27 @@ class albumViewController: UICollectionViewController {
 
         // Do any additional setup after loading the view.
         
-        let viewChangeButton = UIButton(frame: CGRectMake(0, 0, 100, 100))
-        
-        viewChangeButton.center = self.view.center
+        // Button to switch sorting options
+        let viewChangeButton = UIButton(frame: CGRectMake(0, 0, 50, 50))
+        //viewChangeButton.center = self.view.center
         viewChangeButton.center.y = self.view.bounds.height - 50
-        viewChangeButton.setBackgroundImage(UIImage(named:"downloadButton.png"), forState: .Normal)
-        viewChangeButton.addTarget(self, action: "sortButton", forControlEvents: .TouchUpInside)
+        viewChangeButton.backgroundColor = UIColor.whiteColor()
+        viewChangeButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        viewChangeButton.setTitle("Sort", forState: UIControlState.Normal)
+        viewChangeButton.addTarget(self, action: "sortButton", forControlEvents: UIControlEvents.TouchUpInside)
         self.view.addSubview(viewChangeButton)
         self.view.bringSubviewToFront(viewChangeButton)
+        
+        // Button to switch between feed view and album view
+        let feedChangeButton = UIButton(frame: CGRectMake(250,250,50,50))
+        feedChangeButton.center.y = self.view.bounds.height - 50
+        feedChangeButton.backgroundColor = UIColor.whiteColor()
+        feedChangeButton.setTitleColor(UIColor.blackColor(), forState: UIControlState.Normal)
+        feedChangeButton.setTitle("View", forState: UIControlState.Normal)
+        feedChangeButton.addTarget(self, action:"viewChange", forControlEvents: UIControlEvents.TouchUpInside)
+        self.view.addSubview(feedChangeButton)
+        self.view.bringSubviewToFront(feedChangeButton)
+        
 
         
         var getUploadedImages = PFQuery(className: "Post")
@@ -78,15 +77,12 @@ class albumViewController: UICollectionViewController {
                 
                 for object in objects! {
                     
-                    //
+                    // Creates tuple for easier sorting
                     let tup = (image: object["imageFile"] as! PFFile, likes: object["likeCount"] as! Int, id: object.objectId!! as String, title: object["Title"] as! String ,  date: object["timeStamp"] as! NSDate)
                     
                     self.imageFilesTemp.append(tup)
-                    
-                    dump(self.imageFilesTemp)
-                        
-                    
-                    
+    
+                    // Fills arrays for images sorted by time
                     self.imageFilesTime.append(object["imageFile"] as! PFFile)
                     self.objectIDs.append(object.objectId! as String!)
                     self.titles.append(object["Title"] as! String)
@@ -109,16 +105,45 @@ class albumViewController: UICollectionViewController {
     
                 }
                 
-                dump(self.imageFilesLikes)
-                dump(self.imageFilesTime)
-                
             } else {
                 
                 println(error)
                 
             }
         }
+        
     }
+    
+    func sortButton() {
+        
+        // Change boolean, reload data, to sort images
+        if sorted == true {
+        
+        sorted = false
+            
+        self.collectionView?.reloadData()
+        
+        } else {
+        
+        sorted = true
+
+        self.collectionView?.reloadData()
+            
+        }
+        
+    func viewChange() {
+        
+        let storyboard = UIStoryboard(name: "albumView", bundle: nil)
+        let newVC = storyboard.instantiateViewControllerWithIdentifier("feedView") as? feedViewController
+        self.presentViewController(newVC!, animated: true, completion: nil)
+        
+  
+        
+        }
+        
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -153,12 +178,10 @@ class albumViewController: UICollectionViewController {
         let albumCell = collectionView.dequeueReusableCellWithReuseIdentifier(reuseIdentifier, forIndexPath: indexPath) as! albumViewCell
     
         // Configure the cell
-        
 
         if sorted == false {
             
             // Default, fill the cells with photos sorted by time
-        
             imageFilesTime[indexPath.row].getDataInBackgroundWithBlock { (imageData, error) -> Void in
                 
                 if error == nil {
@@ -174,13 +197,10 @@ class albumViewController: UICollectionViewController {
                     
                 }
                 
-                //self.collectionView?.reloadData()
-                
             }
         } else {
             
             // Fill the cells with the sorted photos by likes
-            println(self.imageFilesLikes[indexPath.row])
             imageFilesLikes[indexPath.row].getDataInBackgroundWithBlock { (imageD,error) -> Void in
          
                 if error == nil {
@@ -196,8 +216,6 @@ class albumViewController: UICollectionViewController {
                     
                 }
                 
-                //self.collectionView?.reloadData()
-                
             }
     
        }
@@ -212,13 +230,16 @@ class albumViewController: UICollectionViewController {
             var moveVC: fullScreenViewController = segue.destinationViewController as! fullScreenViewController
             var selectedCellIndex = self.collectionView?.indexPathForCell(sender as! UICollectionViewCell)
             
+            // Sorted by time
             if self.sorted == false {
+                
                 moveVC.cellImage = images[selectedCellIndex!.row]
                 moveVC.objectIdTemp = objectIDs[selectedCellIndex!.row]
                 moveVC.tempDate = dates[selectedCellIndex!.row]
                 moveVC.tempTitle = titles[selectedCellIndex!.row]
-            } else {
                 
+            } else {
+            // Sorted by like count
                 moveVC.cellImage = images[selectedCellIndex!.row]
                 moveVC.objectIdTemp = objectIDTime[selectedCellIndex!.row]
                 moveVC.tempDate = datesTime[selectedCellIndex!.row]
