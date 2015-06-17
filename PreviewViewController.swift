80c83d7f88ef1469ebd965a698aa4222c93002c6
Ivo Bratanov <ivelin.bratanov@mail.mcgill.ca>
2015-06-17 13:48:17 -0400

    photoUpload WIP

--- PreviewViewController.swift
+++ PreviewViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Parse
 
 class PreviewViewController: UIViewController, UIScrollViewDelegate {
     
+    // Title passed from previous VC
+    var eventId : String?
+    var eventTitle : String?
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var imageView: UIImageView!
@@ -155,9 +159,79 @@ class PreviewViewController: UIViewController, UIScrollViewDelegate {
                 self.cropCompletionHandler!(self.imageView.image?.croppedToRect(imageViewRect))
             }
         })
-        UIImageWriteToSavedPhotosAlbum(self.imageView.image?.croppedToRect(imageViewRect), nil, nil, nil)
+        var capturedImage = self.imageView.image?.croppedToRect(imageViewRect) as UIImage!
+        UIImageWriteToSavedPhotosAlbum(capturedImage, nil, nil, nil)
         
         
+        var imageData = compressImage(capturedImage, shrinkRatio: 1.0)
+        var imageFile = PFFile(name: "image.png", data: imageData)
+        
+        var thumbnailData = compressImage(capturedImage, shrinkRatio: 0.5)
+        var thumbnailFile = PFFile(name: "image.png", data: thumbnailData)
+        
+        
+        //Upload photos to database
+        var photo = PFObject(className: "Photo")
+        photo["caption"] = "Camera roll upload"
+        photo["image"] = imageFile
+        photo["thumbnail"] = thumbnailFile
+        photo["upvoteCount"] = 1
+        
+        photo.saveInBackgroundWithBlock { (success, error) -> Void in
+            if (success) {
+                println("PHOTO UPLOADED!------------------")
+            } else {
+                println("FAILED PHOTO UPLOAD!------------------")
+            }
+        }
+        
+        
+    }
+    
+    func compressImage(image:UIImage, shrinkRatio: CGFloat) -> NSData {
+        var imageHeight:CGFloat = image.size.height
+        var imageWidth:CGFloat = image.size.width
+        var maxHeight:CGFloat = 1136.0 * shrinkRatio
+        var maxWidth:CGFloat = 640.0 * shrinkRatio
+        var imageRatio:CGFloat = imageWidth/imageHeight
+        var scalingRatio:CGFloat = maxWidth/maxHeight
+        //lowest quality rating with acceptable encoding
+        var quality:CGFloat = 0.5
+        
+        if (imageHeight > maxHeight || imageWidth > maxWidth){
+            if(imageRatio < scalingRatio){
+                /* To ensure aspect ratio is maintained adjust
+                witdth of image in relation to scaling height */
+                imageRatio = maxHeight / imageHeight;
+                imageWidth = imageRatio * imageWidth;
+                imageHeight = maxHeight;
+            }
+            else if(imageRatio > scalingRatio){
+                /* To ensure aspect ratio is maintained adjust
+                height of image in relation to scaling width */
+                imageRatio = maxWidth / imageWidth;
+                imageHeight = imageRatio * imageHeight;
+                imageWidth = maxWidth;
+            }
+            else{
+                /* If image is equivalent to scaling ratio
+                image should not be compressed any further
+                scaled down to max resolution*/
+                imageHeight = maxHeight;
+                imageWidth = maxWidth;
+                quality = 1;
+            }
+        }
+        
+        var rect = CGRectMake(0.0, 0.0, imageWidth, imageHeight);
+        //bit-map based graphic context and set the boundaries of still image
+        UIGraphicsBeginImageContext(rect.size);
+        image.drawInRect(rect)
+        var imageCompressed = UIGraphicsGetImageFromCurrentImageContext();
+        let imageData = UIImageJPEGRepresentation(imageCompressed, quality);
+        UIGraphicsEndImageContext();
+        
+        return imageData;
     }
     
     @IBAction func cancelButtonPressed(sender: AnyObject) {
