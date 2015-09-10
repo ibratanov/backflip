33b5768f699045e3f31b4e371c00d0c4aba8fb1a
Jack Perry <jack.perry@me.com>
2015-09-10 14:33:53 -0400

    Way better sharing, fixes issue #118

--- Classes/Models/Event+Share.swift
+++ Classes/Models/Event+Share.swift
@@ -6,17 +6,58 @@
 //  Copyright (c) 2015 Backflip. All rights reserved.
 //
 
-import Parse
 import Foundation
 
-class EventShare: NSObject
+
+extension Event : UIActivityItemSource
 {
-//	var objectId : String?
-//	var name : String?
-//	var geoLocation : PFGeoPoint?
-//	var isLive : Boolean?
-//	var startTime : NSDate?
-//	var venue : String?
-//	var photos : [Image]?
+	
+	
+	//-------------------------------------
+	// MARK: UIActivityItemSource
+	//-------------------------------------
+	
+	func activityViewControllerPlaceholderItem(activityViewController: UIActivityViewController) -> AnyObject
+	{
+		return shareText(nil)
+	}
+	
+	func activityViewController(activityViewController: UIActivityViewController, itemForActivityType activityType: String) -> AnyObject?
+	{
+		return shareText(activityType)
+	}
+	
+	func activityViewController(activityViewController: UIActivityViewController, subjectForActivityType activityType: String?) -> String
+	{
+		return shareSubject(activityType)
+	}
+	
+	
+	
+	
+	//-------------------------------------
+	// MARK: Private
+	//-------------------------------------
+	
+	private func shareText(activityType: String?) -> String
+	{
+		if (activityType == UIActivityTypeMail) {
+			return "<html><body>Join me at '"+self.name!+"'"+"</body></html>"
+		} else if (activityType == UIActivityTypePostToTwitter) {
+			return "Join me using @getbackflip at '"+self.name!+"' #backflip"
+		}
+		
+		
+		return "Join me at '"+self.name!+"'"
+	}
+	
+	private func shareSubject(activityType: String?) -> String
+	{
+		if (activityType == UIActivityTypeMail) {
+			return self.name!
+		}
+		
+		return ""
+	}
 	
 }
