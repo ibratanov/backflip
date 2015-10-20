dc56d69e6a8af18fde896b71b755ae0dedef21d4
Jack Perry <jack.perry@me.com>
2015-10-20 17:54:30 -0400

    Fixes for QBImagePicker

--- Classes/Controllers/Camera/BFCameraController.swift
+++ Classes/Controllers/Camera/BFCameraController.swift
@@ -175,7 +175,7 @@ public class BFCameraController : NSObject, UIImagePickerControllerDelegate, UIN
 
 		let multiImagePicker = QBImagePickerController()
 		multiImagePicker.delegate = self
-		multiImagePicker.mediaType = .Image
+		// multiImagePicker.mediaType = .Image
 		multiImagePicker.allowsMultipleSelection = true
 		multiImagePicker.maximumNumberOfSelection = kMULTI_UPLOAD_ASSET_LIMIT
 		multiImagePicker.showsNumberOfSelectedAssets = true
