6edbcb57e0baa45a737b6a2daed63ab136d574be
Ivo Bratanov <ivelin.bratanov@mail.mcgill.ca>
2015-10-27 17:09:27 -0400

    Add download photo to camera roll as per papercut issues (#188)

--- Classes/Controllers/Camera/BFImagePreviewController.swift
+++ Classes/Controllers/Camera/BFImagePreviewController.swift
@@ -160,6 +160,10 @@ public class BFImagePreviewController : UIViewController, UITextViewDelegate
 			comment = nil
 		}
 		
+        if (self.image != nil) {
+            UIImageWriteToSavedPhotosAlbum(self.image!, nil, nil, nil)
+        }
+        
 		delegate?.imagePreviewDidSelectUploadButton?(self, button: uploadButton, image: self.image, comment: comment)
 
 		self.dismissViewControllerAnimated(true, completion: nil)
